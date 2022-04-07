82d36d080fe346117fb489c2a56cf57debef5d8c
ValentinaM <Valen@DESKTOP-R206R25>
2022-04-06 21:47:56 -0300

    6/4 Consulta en base

--- Trabajo Practico 2.1/src/objetos/MainConcurso.java
+++ Trabajo Practico 2.1/src/objetos/MainConcurso.java
@@ -3,6 +3,7 @@ package objetos;
 import java.time.LocalDate;
 
 import persistence.ArchivoRegistroDeInscripcion;
+import persistence.EnBaseRegistroDeInscripcion;
 
 public class MainConcurso {
 
@@ -12,7 +13,7 @@ public class MainConcurso {
 		LocalDate inicio = LocalDate.now().minusDays(3);
 		LocalDate fin = LocalDate.now().plusDays(15);
 
-		Concurso concurso = new Concurso(inicio, fin, 1, 50, new ArchivoRegistroDeInscripcion());
+		Concurso concurso = new Concurso(inicio, fin, 1, 50, new EnBaseRegistroDeInscripcion());
 		Participante participante = new Participante(1);
 		concurso.inscribirse(participante);
 
--- Trabajo Practico 2.1/src/persistence/EnBaseRegistroDeInscripcion.java
+++ Trabajo Practico 2.1/src/persistence/EnBaseRegistroDeInscripcion.java
@@ -1,13 +1,71 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import objetos.RegistroDeInscripcion;
 
 public class EnBaseRegistroDeInscripcion implements RegistroDeInscripcion {
 
+	// TODO Auto-generated method stub
+	private static String DRIVER = "com.mysql.jdbc.Driver";
+	private static String URL_DB = "jdbc:mysql://localhost:3306/";
+	// jdbc:mysql://localhost:3306/
+	protected static String DB = "registro_inscripcion";
+	protected static String user = "root";
+	protected static String pass = "";
+	protected static Connection conn = null;
+
+	private void connect() {
+		try {
+			conn = DriverManager.getConnection(URL_DB + DB + "?useSSL=false", user, pass);
+		} catch (SQLException sqlEx) {
+			System.out.println("No se ha podido conectar a " + URL_DB + DB + ". " + sqlEx.getMessage());
+			System.out.println("Error al cargar el driver");
+		}
+	}
+
+	private void disconnect() {
+		if (conn != null) {
+			try {
+				conn.close();
+				conn = null;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	@Override
 	public void registrarInscripcion(String registro) {
-		// TODO Auto-generated method stub
 
+		//String consulta = "INSERT INTO registro(registro) VALUES (?)";
+		
+		
+		this.connect();
+		
+		
+		
+		try {
+			PreparedStatement statement = conn.prepareStatement(
+					"INSERT INTO registro(registro) "
+			+ "VALUES (?)");
+			statement.setString(1, registro);
+			System.out.println("Se conecto");
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			this.disconnect();
+			System.out.println("Se desconecto");
+		}
+
+	
+		
 	}
 
 	@Override
